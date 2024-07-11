c1c12f741dbdf55f5641287fc9e5a4e8539fa6c3
Dom Rodriguez <shymega@shymega.org.uk>
2024-07-11 17:16:20 +0100

    feat: Initial REUSE compliance
    Flatpak's Cargo tool is not compliant. Working on that with upstream.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+// SPDX-FileCopyrightText: 2024 The DeckCheatz Developers
+// SPDX-License-Identifier: Apache-2.0
+
+use color_eyre::eyre::{Result, Report};
+
+fn setup_eyre() -> Result<()> {
+    Ok(color_eyre::config::HookBuilder::default()
+        .issue_url(concat!(env!("CARGO_PKG_REPOSITORY"), "/issues/new"))
+        .add_issue_metadata("version", env!("CARGO_PKG_VERSION"))
+        .install()?)
+}
+#[tokio::main]
+async fn main() -> Result<(), Report>{
+    setup_eyre()?;
+
+    Ok(())
 }
