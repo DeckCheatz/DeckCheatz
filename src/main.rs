57f32d81a455729ee2d92b39cba6326237b42d06
Dom Rodriguez <shymega@shymega.org.uk>
2026-02-07 15:34:38 +0000

    refactor: Trim down to barebones layout for new structure

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 // SPDX-FileCopyrightText: 2024-2026 The DeckCheatz Developers
 // SPDX-License-Identifier: AGPL-3.0-only
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui;
 use log::debug;
-use std::env;
-use std::fs::canonicalize;
-use std::path::PathBuf;
-use wincompatlib::prelude::*;
 
 use env_logger::{Builder as EnvLoggerBuilder, Env};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-const CUSTOM_PROTON: (&str, &str) = ("GE-Proton9-1", "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton9-1/GE-Proton9-1.tar.gz");
 
 fn init_app() -> Result<()> {
     EnvLoggerBuilder::from_env(Env::default().default_filter_or("info")).init();
@@ -21,18 +13,6 @@ fn init_app() -> Result<()> {
 
     debug!("Base app initialized.");
     Ok(())
-}
-
-fn get_proton() -> Result<Proton> {
-    let prefix_dir = PathBuf::from(env::var("STEAM_COMPAT_DATA_PATH")?);
-    let proton_dir = canonicalize(&PathBuf::from(
-        "~/.local/share/Steam/compatibilitytools.d/GE-Proton9-1",
-    ))?;
-
-    let proton = Proton::new(proton_dir, None).with_prefix(prefix_dir);
-    proton.update_prefix(None::<&str>)?;
-
-    Ok(proton)
 }
 
 #[tokio::main]
@@ -43,23 +23,6 @@ async fn main() -> Result<()> {
 
     // Logging initialized.
     // Error handling initialised.
-
-    let proton = get_proton()?;
-    proton.wine().run("cmd")?;
-
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
-        ..Default::default()
-    };
-
-    _ = eframe::run_simple_native("DeckCheatz", options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("DeckCheatz");
-            ui.label("Welcome to DeckCheatz!");
-            ui.label("This is a work in progress.");
-            ui.label("Please check back later.");
-        });
-    });
 
     Ok(())
 }
